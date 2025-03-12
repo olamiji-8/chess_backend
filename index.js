6a6e9e87ccec8cfc4caf37d4d0b6c97740be4176
olamiji-8 <olamijisotomi@gmail.com>
2025-03-12 21:49:21 +0100

    updated

--- index.js
+++ index.js
@@ -131,4 +131,8 @@ app.use((err, req, res, next) => {
 });
 
 // Export for Vercel
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 module.exports = app;
